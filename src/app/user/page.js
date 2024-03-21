a2feaa021f53d52f2fad6adf2ef669a5b1981f41
Nicole Franklin <Ncfranklin14@gmail.com>
2024-03-21 08:56:54 -0700

    homepage structure and testing scss

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -1,5 +1,33 @@
+'use client'
+import { useState } from "react";
+import styles from '../../styles/User.module.scss';
+
 export default function User() {
-    return (
-   <h1>User Home</h1>
-    );
-  }
+  const [moduleList, setModuleList] = useState(["module1", "module2", "module3"])
+  return (
+    <div className="main">
+      <div className={styles.header}>
+        <div className="headerTitle">
+          <h1>Header Title</h1>
+        </div>
+        <div className="headerMenu">
+          menu
+        </div>
+      </div>
+      <div className="body">
+        <div className="bodyHeaderWrapper">
+          <div className="profilePic">
+            <p>this will be an img</p>
+          </div>
+        </div>
+        <div className="bodyModuleWrapper">
+          {moduleList.map((module) => (
+            <div key={module}>
+              {module}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
