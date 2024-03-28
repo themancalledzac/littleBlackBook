737be994c4aabfa0751d18a4527c451bb6ce1d53
Zac Edens <edens.zac@gmail.com>
2024-03-27 20:36:24 -0700

    LBB-0009 initial commit and yolo merge

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -1,33 +1,51 @@
 'use client'
 import { useState } from "react";
-import styles from '../../styles/User.module.scss';
+import styles from './user.module.scss';
+import Header from "@/Components/Header";
+import DropdownMenu from "@/Components/DropdownMenu";
 
+/**
+ * Default Page for Logged in User.
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export default function User() {
-  const [moduleList, setModuleList] = useState(["module1", "module2", "module3"])
-  return (
-    <div className="main">
-      <div className={styles.header}>
-        <div className="headerTitle">
-          <h1>Header Title</h1>
-        </div>
-        <div className="headerMenu">
-          menu
-        </div>
-      </div>
-      <div className="body">
-        <div className="bodyHeaderWrapper">
-          <div className="profilePic">
-            <p>this will be an img</p>
-          </div>
-        </div>
-        <div className="bodyModuleWrapper">
-          {moduleList.map((module) => (
-            <div key={module}>
-              {module}
+    const [moduleList, setModuleList] = useState( ["module1", "module2", "module3"] )
+    const [dropdownMenuOpen, setDropdownMenuOpen] = useState( false );
+    return (
+        <div className="main">
+            <Header
+                dropdownMenuOpen={dropdownMenuOpen} // pass header dropdown boolean value as param
+                setDropdownMenuOpen={setDropdownMenuOpen} // pass header dropdown function as param
+            />
+            <div className="body">
+                <div className="bodyHeaderWrapper">
+                    <div className="profilePic">
+                        <p>this will be an img</p>
+                    </div>
+                </div>
+                <div className="bodyModuleWrapper">
+                    {moduleList.map( ( module ) => (
+                        <div key={module}>
+                            {module}
+                        </div>
+                    ) )}
+                </div>
             </div>
-          ))}
+            {dropdownMenuOpen && (
+                <DropdownMenu/>
+            )}
         </div>
-      </div>
-    </div>
-  );
+    );
 }
+
+
+const contact = [
+    {name: 'beyonce', img: 'something', phone: 'illegal'},
+    {name: 'beyonce', img: 'something', phone: 'illegal'},
+    {name: 'beyonce', img: 'something', phone: 'illegal'},
+];
+
+
+Card( contact );
