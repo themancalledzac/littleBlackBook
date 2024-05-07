cee91047194395aeff288a4494647a1a1768a51d
Nicole Franklin <Ncfranklin14@gmail.com>
2024-05-06 17:46:15 -0700

    remove console log

--- src/Components/ListSimpleItem/ListSimpleItem.jsx
+++ src/Components/ListSimpleItem/ListSimpleItem.jsx
@@ -2,7 +2,6 @@
 import { useState } from "react";
 
 export default function ListSimpleItem({ groceryItem }) {
-    console.log(groceryItem);
     const [isStrikethrough, setIsStriketrough] = useState(false);
 
     const toggleStrikethrough = () => {
