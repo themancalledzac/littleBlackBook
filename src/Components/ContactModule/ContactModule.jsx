0aa26b3e4431aa33b304830be3c3d6c403385544
Nicole Franklin <Ncfranklin14@gmail.com>
2024-08-13 20:10:58 -0700

    built form and addded it to the contactModule

--- src/Components/ContactModule/ContactModule.jsx
+++ src/Components/ContactModule/ContactModule.jsx
@@ -2,11 +2,13 @@
 
 import styles from './ContactModule.module.scss';
 import ContactItem from "@/Components/ContactItem/ContactItem";
+import CreateContactModule from "@/Components/CreateContactModule/CreateContactModule";
 import { useState } from "react";
 import { Plus } from "lucide-react";
 
 export default function ContactModule({ module }) {
     const [selectedContact, setSelectedContact] = useState(null);
+    const [createNew, setCreateNew] = useState(false);
     // const selectedContact = module.data.find( contact => contact.id === currentChoice );
 
 
@@ -21,24 +23,26 @@ export default function ContactModule({ module }) {
 
     return (
         <div className={styles.contactModuleWrapper}>
-            {selectedContact == null ? (
+            {selectedContact !== null ? (
+                <ContactItem moduleItem={selectedContact} setSelectedContact={setSelectedContact} />
+            ) : (createNew ? (
+                <CreateContactModule setCreateNew={setCreateNew}  />
+            ) : (
+
                 <div>
                     <div className={styles.moduleHeader}>
                         <h1 className={styles.moduleTitle}>{module.title}</h1>
-                        <Plus className={styles.lucide} />
+                        <Plus className={styles.lucide} onClick={() => setCreateNew(!createNew)}/>
                     </div>
                     <div className={styles.dataList}>
                         {module.data.map((contact) => (
                             <div id={contact.uuid} className={styles.dataItem} onClick={() => updateSelectedContact(contact.id)}>
                                 <h2>{contact.title}</h2>
-                                </div>
+                            </div>
                         ))}
                     </div>
                 </div>
-            ) : (
-                <ContactItem moduleItem={selectedContact} setSelectedContact={setSelectedContact} />
-
-            )}
+            ))}
 
         </div>
     );
